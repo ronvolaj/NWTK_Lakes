41a49bc8d678f20e717472a36915a5d9d5a8201e
ronvolaj <ronvolaj8@gmail.com>
2026-03-12 15:59:35 +0100

    Add GET /api/lakes endpoint

--- src/routes/api/Lakes/+server.js
+++ src/routes/api/Lakes/+server.js
@@ -1,12 +1,13 @@
 import pool from '$lib/server/database';
 
-import pool from '$lib/server/database.js';
+import { API_USER, API_PASS } from '$env/static/private';
 
 function checkAuth(request) {
-    const auth = request.headers.get('authorization');
-    if (!auth || !auth.startsWith('Basic ')) return false;
-
-    const [user, pass] = atob(auth.split(' ')[1]).split(':');
-    return user === env.API_USER && pass === env.API_PASSWORD;
+    const auth = request.headers.get('Authorization');
+    if (!auth?.startsWith('Basic ')) return false;
+    const base64 = auth.slice(6);
+    const decoded = atob(base64);
+    const [user, pass] = decoded.split(':');
+    return user === API_USER && pass === API_PASS;
 }
 
